5d97762bf668b8ea580566014212f20cdd25cd00
hgirish <girish@post.com>
2024-07-24 16:53:27 -0700

    entities

---
+++ WorldCities.Server/Data/Models/City.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorldCities.Server.Data.Models;
+
+[Table("Cities")]
+[Index(nameof(Name))]
+[Index(nameof(Lat))]
+[Index(nameof(Lon))]
+public class City
+{
+    #region Properties
+    /// <summary>
+    /// The unique id and primary key for this city
+    /// </summary>
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// City name (in UTF8 format)
+    /// </summary>
+    [Required] public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// City Latitude
+    /// </summary>
+    [Column(TypeName = "decimal(7,4)")]
+    public decimal Lat { get; set; }
+
+    /// <summary>
+    /// City Lognitude
+    /// </summary>
+    [Column(TypeName = "decimal(7,4)")]
+    public decimal Lon { get; set; }
+
+    /// <summary>
+    /// Country Id (foreign key)
+    /// </summary>
+    [ForeignKey(nameof(Country))]
+    public int CountryId { get; set; }
+    #endregion
+
+    #region Navigation Properties
+    /// <summary>
+    /// The country related to this city
+    /// </summary>
+    public Country? Country { get; set; }
+}
---
+++ WorldCities.Server/Data/Models/Country.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorldCities.Server.Data.Models;
+
+[Table("Countries")]
+[Index(nameof(Name))]
+[Index(nameof(ISO2))]
+[Index(nameof(ISO3))]
+public class Country
+{
+    #region Properties
+    /// <summary>
+    /// The unique id and primary key for this city
+    /// </summary>
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Country name (in UTF8 format)
+    /// </summary>
+    public required string Name { get; set; } = string.Empty;
+
+
+
+    /// <summary>
+    /// Country code (in ISO 3166-1 ALPHA-2 format)
+    /// </summary>
+    public required string ISO2 { get; set; }
+    /// <summary>
+    /// Country code (in ISO 3166-1 ALPHA-3 format)
+    /// </summary>
+    public required string ISO3 { get; set; }
+    #endregion
+
+    #region Navigation Properties
+    /// <summary>
+    /// A collection of all the cities related to this country
+    /// </summary>
+    public ICollection<City>? Cities { get; set; }
+    #endregion
+}
