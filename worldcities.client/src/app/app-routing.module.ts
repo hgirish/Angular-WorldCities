d1fbc23b5a0953da9c5011897c7bb57fcc00e44e
Girish <girish@post.com>
2024-07-24 13:40:21 -0700

    fix routing

--- worldcities.client/src/app/app-routing.module.ts
+++ worldcities.client/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  { path: '', component: HomeComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
