aec3cf4e396ee52ef381d1efc4977ac683621c6d
Girish <girish@post.com>
2024-07-31 09:34:42 -0700

    offline

--- worldcities.client/src/app/app.component.ts
+++ worldcities.client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { Subscription, tap } from 'rxjs';
+import { ConnectionService, ConnectionServiceOptions, ConnectionState } from 'ng-connection-service';
+import { environment } from '../environments/environment';
 
 interface WeatherForecast {
   date: string;
@@ -16,9 +19,39 @@ interface WeatherForecast {
 export class AppComponent implements OnInit  {
   title = 'World Cities';
 
-  constructor(private authService: AuthService) { }
+  status!: string;
+  currentState!: ConnectionState;
+  subscription = new Subscription();
+
+  constructor(private authService: AuthService,
+    private connectionService: ConnectionService) { }
 
   ngOnInit(): void {
     this.authService.init();
+    this.checkConnectionStatus();
+  }
+
+  checkConnectionStatus(): void {
+    const options: ConnectionServiceOptions = {
+      enableHeartbeat: true,
+      heartbeatUrl: environment.baseUrl + 'api/heartbeat',
+      heartbeatInterval: 10000
+    };
+    this.subscription.add(
+      this.connectionService.monitor(options).pipe(
+        tap((newState: ConnectionState) => {
+          this.currentState = newState;
+
+          if (this.currentState.hasNetworkConnection && this.currentState.hasInternetAccess) {
+            this.status = 'ONLINE';
+
+          } else {
+            this.status = 'OFFLINE';
+
+          }
+          console.log('status', this.status);
+        })
+      ).subscribe()
+    );
   }
 }
